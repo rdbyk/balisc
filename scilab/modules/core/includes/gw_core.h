96b85d8aa672153b344bbb3bb63dd3c8609bd95c
rdbyk <rdbyk@users.noreply.github.com>
2017-06-07 13:51:00 +0200

    removed sci_debug.c

--- scilab/modules/core/includes/gw_core.h
+++ scilab/modules/core/includes/gw_core.h
@@ -1,9 +1,9 @@
 /*
  * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
  * Copyright (C) 2006 - INRIA - Allan CORNET
- *
  * Copyright (C) 2012 - 2016 - Scilab Enterprises
- *
+ * Copyright (C) 2017 - Dirk Reusch, Kybernetik Dr. Reusch
+ * 
  * This file is hereby licensed under the terms of the GNU GPL v2.0,
  * pursuant to article 5.3.4 of the CeCILL v.2.1.
  * This file was originally licensed under the terms of the CeCILL v2.1,
@@ -20,22 +20,15 @@
 /*--------------------------------------------------------------------------*/
 int gw_core(void);
 /*--------------------------------------------------------------------------*/
-STACK_GATEWAY_PROTOTYPE(C2F(sci_debug));
 int C2F(sci_who)(char *fname, unsigned long fname_len);
-//int C2F(sci_warning)(char *fname,unsigned long fname_len); -> C++
-//int C2F(sci_mode)(char *fname, unsigned long fname_len); -> c++
-//int C2F(sci_error)(char *fname,unsigned long fname_len); -> C++
 int C2F(sci_resume)(char *fname, unsigned long fname_len);
 int C2F(sci_return)(char *fname, unsigned long fname_len);
 int C2F(sci_isdef)(char *fname, unsigned long fname_len);
-//int C2F(sci_errclear)(char *fname, unsigned long fname_len); -> C++
 STACK_GATEWAY_PROTOTYPE(C2F(sci_predef));
 int C2F(sci_clearfun)(char *fname, unsigned long fname_len);
 int C2F(sci_funptr)(char *fname, unsigned long fname_len);
-//int C2F(sci_macr2lst)(char *fname, unsigned long fname_len);
 int C2F(sci_whereis)(char *fname, unsigned long fname_len);
 int C2F(sci_where)(char *fname, void* pvApiCtx);
-//int C2F(sci_stacksize)(char *fname, unsigned long fname_len);
 int C2F(sci_mtlb_mode)(char *fname, unsigned long fname_len);
 int C2F(sci_what)(char *fname, unsigned long fname_len);
 int C2F(sci_ieee)(char *fname, unsigned long fname_len);
@@ -43,16 +36,11 @@ int C2F(sci_getos)(char *fname, unsigned long fname_len);
 int C2F(sci_fromjava)(char *fname, unsigned long fname_len);
 int C2F(sci_getmemory)(char *fname, unsigned long fname_len);
 int C2F(sci_fromc)(char *fname, unsigned long fname_len);
-//int C2F(sci_getmd5)(char *fname,unsigned long fname_len); -> C++
 int C2F(sci_getmodules)(char *fname, unsigned long fname_len);
 int C2F(sci_with_module)(char *fname, unsigned long fname_len);
 int C2F(sci_getscilabmode)(char *fname, unsigned long fname_len);
 int C2F(sci_librarieslist)(char *fname, unsigned long fname_len);
-//int C2F(sci_getvariablesonstack)(char *fname, unsigned long fname_len);
-
-//STACK_GATEWAY_PROTOTYPE(sci_typename); -> c++
 int C2F(sci_funcprot)(char *fname, void* pvApiCtx);
-//int C2F(sci_newfun)(char *fname, void* pvApiCtx); -> C++
 
 //Scilab 6
 STACK_GATEWAY_PROTOTYPE(sci_getdebuginfo);
