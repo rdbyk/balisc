d5b4e1cf740f8e85140559b9500c5c6dbabcc628
rdbyk <rdbyk@users.noreply.github.com>
2017-06-07 13:53:27 +0200

    removed sci_global.c, sci_ieee.c

--- scilab/modules/core/includes/gw_core.h
+++ scilab/modules/core/includes/gw_core.h
@@ -31,7 +31,6 @@ int C2F(sci_whereis)(char *fname, unsigned long fname_len);
 int C2F(sci_where)(char *fname, void* pvApiCtx);
 int C2F(sci_mtlb_mode)(char *fname, unsigned long fname_len);
 int C2F(sci_what)(char *fname, unsigned long fname_len);
-int C2F(sci_ieee)(char *fname, unsigned long fname_len);
 int C2F(sci_getos)(char *fname, unsigned long fname_len);
 int C2F(sci_fromjava)(char *fname, unsigned long fname_len);
 int C2F(sci_getmemory)(char *fname, unsigned long fname_len);
