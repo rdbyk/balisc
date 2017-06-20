1d7cbe0a6b961c7a2b5e52a912956af28d9e87d2
rdbyk <rdbyk@users.noreply.github.com>
2017-06-20 09:31:50 +0200

    clean-up part implementation

--- scilab/modules/string/src/c/partfunction.c
+++ scilab/modules/string/src/c/partfunction.c
@@ -1,9 +1,8 @@
-
 /*
  * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
  * Copyright (C) 2008 - INRIA - Allan CORNET
- *
  * Copyright (C) 2012 - 2016 - Scilab Enterprises
+ * Copyright (C) 2017 - Dirk Reusch, Kybernetik Dr. Reusch
  *
  * This file is hereby licensed under the terms of the GNU GPL v2.0,
  * pursuant to article 5.3.4 of the CeCILL v.2.1.
@@ -14,77 +13,23 @@
  *
  */
 
-/*--------------------------------------------------------------------------*/
 #include <string.h>
 #include "sci_malloc.h"
 #include "partfunction.h"
 #include "freeArrayOfString.h"
 #include "charEncoding.h"
 
-/*--------------------------------------------------------------------------*/
-#define BLANK_CHAR ' '
-/*--------------------------------------------------------------------------*/
-char **partfunction(char** stringInput, int m, int n, int *vectInput, int row)
-{
-    char **parts = NULL;
-    int mn = m * n;
-
-    int i = 0;
-
-    parts = (char**)MALLOC(sizeof(char*) * (mn));
-
-    for (i = 0; i < mn; i++)
-    {
-        int j = 0;
-        int lengthstringInput = 0;
-        wchar_t *wcInput = to_wide_string(stringInput[i]);
-        wchar_t *wcOutput = NULL;
-
-        wcOutput = (wchar_t*)MALLOC(sizeof(wchar_t) * ((row) + 1));
-
-        if (wcInput)
-        {
-            lengthstringInput = (int)wcslen(wcInput);
-            for (j = 0; j < row; j++)
-            {
-                if ( vectInput[j] > lengthstringInput )
-                {
-                    wcOutput[j] = L' ';
-                }
-                else
-                {
-                    wcOutput[j] = wcInput[vectInput[j] - 1];
-                }
-            }
-            FREE(wcInput);
-        }
-        else // fill output with blank
-        {
-            for (j = 0; j < row; j++)
-            {
-                wcOutput[j] = L' ';
-            }
-        }
-
-        wcOutput[j] = '\0';
-        parts[i] = wide_string_to_UTF8(wcOutput);
-        FREE(wcOutput);
-    }
-    return parts;
-}
-/*--------------------------------------------------------------------------*/
 wchar_t **partfunctionW(wchar_t** _pwstStringInput, int _iRows, int _iCols, int *_piVectInput, int _iVectSize)
 {
-    int i, j;
-    wchar_t **pwstParts = NULL;
     int iSize = _iRows * _iCols;
+    wchar_t** pwstParts = (wchar_t**)MALLOC(sizeof(wchar_t*) * (iSize));
 
-    pwstParts = (wchar_t**)MALLOC(sizeof(wchar_t*) * (iSize));
-
+    int i;
     for (i = 0 ; i < iSize ; i++)
     {
         pwstParts[i] = (wchar_t*)MALLOC(sizeof(wchar_t) * (_iVectSize + 1));
-
+        
+        int j;
         for (j = 0 ; j < _iVectSize ; j++)
         {
             if (_piVectInput[j] > wcslen(_pwstStringInput[i]))
@@ -96,8 +41,8 @@ wchar_t **partfunctionW(wchar_t** _pwstStringInput, int _iRows, int _iCols, int 
                 pwstParts[i][j] = _pwstStringInput[i][_piVectInput[j] - 1];
             }
         }
-        pwstParts[i][j] = '\0';
+        pwstParts[i][_iVectSize] = L'\0';
     }
     return pwstParts;
 }
-/*--------------------------------------------------------------------------*/
+
