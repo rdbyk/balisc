ffcaaa7779cb3244b07a8e25d0451f99a392130a
rdbyk <rdbyk@users.noreply.github.com>
2017-07-03 13:43:27 +0200

    save strlen call(s) in XMLAttr.cpp

--- scilab/modules/xml/src/cpp/XMLAttr.cpp
+++ scilab/modules/xml/src/cpp/XMLAttr.cpp
@@ -1,8 +1,8 @@
 /*
  * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
  * Copyright (C) 2012 - Scilab Enterprises - Calixte DENIZET
- *
  * Copyright (C) 2012 - 2016 - Scilab Enterprises
+ * Copyright (C) 2017 - Dirk Reusch, Kybernetik Dr. Reusch
  *
  * This file is hereby licensed under the terms of the GNU GPL v2.0,
  * pursuant to article 5.3.4 of the CeCILL v.2.1.
@@ -125,7 +125,7 @@ void XMLAttr::setAttributeValue(xmlNode * node, const char *prefix, const char *
         {
             xmlNs *ns = 0;
 
-            if (!strncmp(prefix, "http://", strlen("http://")))
+            if (!strncmp(prefix, "http://", /* strlen("http://") */ 7))
             {
                 ns = xmlSearchNsByHref(node->doc, node, (const xmlChar *)prefix);
             }
