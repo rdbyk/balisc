4c63d13f43a9b150e15961d68a5d8eb0879dda66
rdbyk <rdbyk@users.noreply.github.com>
2017-07-03 13:56:35 +0200

    save strlen call(s) in sci_xmlElement.cpp

--- scilab/modules/xml/sci_gateway/cpp/sci_xmlElement.cpp
+++ scilab/modules/xml/sci_gateway/cpp/sci_xmlElement.cpp
@@ -1,8 +1,8 @@
 /*
  * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
  * Copyright (C) 2011 - Scilab Enterprises - Calixte DENIZET
- *
  * Copyright (C) 2012 - 2016 - Scilab Enterprises
+ * Copyright (C) 2017 - Dirk Reusch, Kybernetik Dr. Reusch
  *
  * This file is hereby licensed under the terms of the GNU GPL v2.0,
  * pursuant to article 5.3.4 of the CeCILL v.2.1.
@@ -82,7 +82,7 @@ int sci_xmlElement(char *fname, void* pvApiCtx)
         return 0;
     }
 
-    if (!strlen(name) || xmlValidateName((const xmlChar *)name, 0))
+    if ((name[0] == '\0') || xmlValidateName((const xmlChar *)name, 0))
     {
         freeAllocatedSingleString(name);
         Scierror(999, gettext("%s: Bad input argument #%d: A valid XML name expected.\n"), fname, 2);
