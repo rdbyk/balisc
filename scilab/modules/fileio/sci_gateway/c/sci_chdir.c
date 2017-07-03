5d10bbb990d74108d03430b6d3ad2af93675f7aa
rdbyk <rdbyk@users.noreply.github.com>
2017-07-03 10:44:34 +0200

    save wcslen call(s) in  sci_chdir.c

--- scilab/modules/fileio/sci_gateway/c/sci_chdir.c
+++ scilab/modules/fileio/sci_gateway/c/sci_chdir.c
@@ -2,8 +2,8 @@
  * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
  * Copyright (C) 2006 - INRIA - Allan CORNET
  * Copyright (C) 2009 - DIGITEO - Allan CORNET
- *
  * Copyright (C) 2012 - 2016 - Scilab Enterprises
+ * Copyright (C) 2017 - Dirk Reusch, Kybernetik Dr. Reusch
  *
  * This file is hereby licensed under the terms of the GNU GPL v2.0,
  * pursuant to article 5.3.4 of the CeCILL v.2.1.
@@ -41,7 +41,7 @@ int sci_chdir(char *fname, void* pvApiCtx)
 
     if (Rhs == 0)
     {
-        pStVarOne = (wchar_t*)MALLOC(sizeof(wchar_t) * ((int)wcslen(L"home") + 1));
+        pStVarOne = (wchar_t*)MALLOC(sizeof(wchar_t) * (/* (int)wcslen(L"home") */ 4 + 1));
         if (pStVarOne)
         {
             wcscpy(pStVarOne, L"home");
