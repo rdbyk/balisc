053f2a202a268dc0c1e22e4a29600d4687909beb
rdbyk <rdbyk@users.noreply.github.com>
2017-07-04 13:00:14 +0200

    use simd strcmp in XMLDocFieldsGetter.cpp

--- scilab/modules/completion/src/cpp/XMLDocFieldsGetter.cpp
+++ scilab/modules/completion/src/cpp/XMLDocFieldsGetter.cpp
@@ -1,8 +1,8 @@
 /*
  * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
  * Copyright (C) 2013 - Scilab Enterprises - Calixte DENIZET
- *
  * Copyright (C) 2012 - 2016 - Scilab Enterprises
+ * Copyright (C) 2017 - Dirk Reusch, Kybernetik Dr. Reusch
  *
  * This file is hereby licensed under the terms of the GNU GPL v2.0,
  * pursuant to article 5.3.4 of the CeCILL v.2.1.
@@ -18,6 +18,7 @@
 extern "C"
 {
 #include "api_scilab.h"
+#include "strcmp.h"
 }
 
 #include "FieldsManager.hxx"
@@ -50,7 +51,7 @@ const char ** XMLDocFieldsGetter::getFieldsName(const org_modules_xml::XMLDocume
         return XMLFieldsGetter::copy(fieldsName, 2);
     }
 
-    if (!strcmp(fieldPath[0], "root"))
+    if (!balisc_strcmp(fieldPath[0], "root"))
     {
         return XMLElemFieldsGetter::getFieldsName(doc->getRoot(), fieldPath + 1, fieldPathLen - 1, fieldsSize);
     }
