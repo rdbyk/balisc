d5b41385c709f288c456864493305e93750f2108
rdbyk <rdbyk@users.noreply.github.com>
2017-07-04 13:02:43 +0200

    use simd strcmp in UnknownMlistFieldsGetter.cpp

--- scilab/modules/completion/src/cpp/UnknownMlistFieldsGetter.cpp
+++ scilab/modules/completion/src/cpp/UnknownMlistFieldsGetter.cpp
@@ -1,8 +1,8 @@
 /*
  * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
  * Copyright (C) 2013 - Scilab Enterprises - Calixte DENIZET
- *
  * Copyright (C) 2012 - 2016 - Scilab Enterprises
+ * Copyright (C) 2017 - Dirk Reusch, Kybernetik Dr. Reusch
  *
  * This file is hereby licensed under the terms of the GNU GPL v2.0,
  * pursuant to article 5.3.4 of the CeCILL v.2.1.
@@ -21,6 +21,7 @@ extern "C"
 #include "freeArrayOfString.h"
 #include "api_scilab.h"
 #include "sci_malloc.h"
+#include "strcmp.h"
 }
 
 namespace org_modules_completion
@@ -87,7 +88,7 @@ const char ** UnknownMlistFieldsGetter::getFieldsName(int * mlist, char ** field
 
     for (int i = 1; i < rc; i++)
     {
-        if (!strcmp(pstData[i], fieldPath[0]))
+        if (!balisc_strcmp(pstData[i], fieldPath[0]))
         {
             freeArrayOfString(pstData, rc);
             int * itemAddr = 0;
