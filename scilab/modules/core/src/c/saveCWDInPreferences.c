5eebe61e13228de561c36a6fe71973ff5e2ec10a
rdbyk <rdbyk@users.noreply.github.com>
2017-07-04 10:54:03 +0200

    use simd strcmp in saveCWDInPreferences.c

--- scilab/modules/core/src/c/saveCWDInPreferences.c
+++ scilab/modules/core/src/c/saveCWDInPreferences.c
@@ -1,8 +1,8 @@
 /*
  * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
  * Copyright (C) 2014 - Scilab Enterprises - Calixte DENIZET
- *
  * Copyright (C) 2012 - 2016 - Scilab Enterprises
+ * Copyright (C) 2017 - Dirk Reusch, Kybernetik Dr. Reusch
  *
  * This file is hereby licensed under the terms of the GNU GPL v2.0,
  * pursuant to article 5.3.4 of the CeCILL v.2.1.
@@ -19,6 +19,7 @@
 #include "os_string.h"
 #include "scicurdir.h"
 #include "sci_malloc.h"
+#include "strcmp.h"
 
 void saveCWDInPreferences()
 {
@@ -32,7 +33,7 @@ void saveCWDInPreferences()
             int err;
             char * cwd = scigetcwd(&err);
 
-            if (!err && cwd && values[1] && strcmp(cwd, values[1]))
+            if (!err && cwd && values[1] && balisc_strcmp(cwd, values[1]))
             {
                 const char * kv[] = {"previous", cwd};
                 setPrefAttributesValues("//general/body/startup", kv, sizeof(kv) / sizeof(const char *));
