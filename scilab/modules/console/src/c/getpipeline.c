b5a736cb193c8176d60db29701708da0b64dca68
rdbyk <rdbyk@users.noreply.github.com>
2017-07-05 14:07:04 +0200

    use simd strlen in getpipeline.c

--- scilab/modules/console/src/c/getpipeline.c
+++ scilab/modules/console/src/c/getpipeline.c
@@ -1,22 +1,23 @@
 /*
-* Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
-* Copyright (C) 2016 - Scilab Enterprises - Cedric Delamarre
-* Copyright (C) 2012 - 2016 - Scilab Enterprises
-*
-* This file is hereby licensed under the terms of the GNU GPL v2.0,
-* pursuant to article 5.3.4 of the CeCILL v.2.1.
-* This file was originally licensed under the terms of the CeCILL v2.1,
-* and continues to be available under such terms.
-* For more information, see the COPYING file which you should have received
-* along with this program.
-* === LICENSE_END ===
-*
-*/
+ * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ * Copyright (C) 2016 - Scilab Enterprises - Cedric Delamarre
+ * Copyright (C) 2012 - 2016 - Scilab Enterprises
+ * Copyright (C) 2017 - Dirk Reusch, Kybernetik Dr. Reusch
+ * 
+ * This file is hereby licensed under the terms of the GNU GPL v2.0,
+ * pursuant to article 5.3.4 of the CeCILL v.2.1.
+ * This file was originally licensed under the terms of the CeCILL v2.1,
+ * and continues to be available under such terms.
+ * For more information, see the COPYING file which you should have received
+ * along with this program.
+ *
+ */
 #include <stdio.h>
 #include "isatty.hxx"
 #include "os_string.h"
 #include "machine.h" // bsiz
 #include "getpipeline.h"
+#include "strlen.h"
 
 char* getPipeLine(void)
 {
@@ -32,7 +33,7 @@ char* getPipeLine(void)
     }
 
     //remove trailing \n
-    len_line = (int)strlen(buffer);
+    len_line = (int)basic_strlen(buffer);
     if (buffer[len_line - 1] == '\n')
     {
         buffer[len_line - 1] = '\0';
