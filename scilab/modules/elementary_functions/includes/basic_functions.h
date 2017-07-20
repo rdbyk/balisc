5dc993bb5a7f8bc9f48912e27f37b09a93b98a5d
rdbyk <rdbyk@users.noreply.github.com>
2017-07-20 19:16:01 +0200

    removed obsolete function dfrexps

--- scilab/modules/elementary_functions/includes/basic_functions.h
+++ scilab/modules/elementary_functions/includes/basic_functions.h
@@ -37,7 +37,6 @@ ELEMENTARY_FUNCTIONS_IMPEXP void vDless(int _iNbElem, double* _pdblIn1, double* 
 ELEMENTARY_FUNCTIONS_IMPEXP void vDset(int _iNbElem, double _dblVal, double* _pdblIn, int _iInc);
 ELEMENTARY_FUNCTIONS_IMPEXP void vDsearchC(double *_pdblX, int _iNbElemX, double *_pdblVal, int _iNbElemVal, double* _pdblInd, double *_pdblOcc, double *_pdblInfo);
 ELEMENTARY_FUNCTIONS_IMPEXP void vDsearchD(double *_pdblX, int _iNbElemX, double *_pdblVal, int _iNbElemVal, double* _pdblInd, double *_pdblOcc, double *_pdblInfo);
-ELEMENTARY_FUNCTIONS_IMPEXP double dfrexps(double _dblVal, double *_pdblExp);
 
 ELEMENTARY_FUNCTIONS_IMPEXP double durands(int *_iVal);
 ELEMENTARY_FUNCTIONS_IMPEXP int nint(double _iVal);
