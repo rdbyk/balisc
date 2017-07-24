03f935cd718174a16874fe98339371bf1ae37ee7
rdbyk <rdbyk@users.noreply.github.com>
2017-07-24 11:53:16 +0200

    reduce usage of std::endl in H5GroupsList.cpp

--- scilab/modules/hdf5/src/cpp/H5GroupsList.cpp
+++ scilab/modules/hdf5/src/cpp/H5GroupsList.cpp
@@ -1,8 +1,8 @@
 /*
  * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
  * Copyright (C) 2012 - Scilab Enterprises - Calixte DENIZET
- *
  * Copyright (C) 2012 - 2016 - Scilab Enterprises
+ * Copyright (C) 2017 - Dirk Reusch, Kybernetik Dr. Reusch
  *
  * This file is hereby licensed under the terms of the GNU GPL v2.0,
  * pursuant to article 5.3.4 of the CeCILL v.2.1.
@@ -109,7 +109,7 @@ std::string H5GroupsList::dump(const unsigned int indentLevel) const
     for (unsigned int i = 0; i < size; i++)
     {
         const H5Group & group = const_cast<H5GroupsList *>(this)->getObject(i, false);
-        os << group.toString(indentLevel) << std::endl;
+        os << group.toString(indentLevel) << '\n';
 
         delete &group;
     }
