9897696ff53c9c060d549a270380fc70e2bc7e48
rdbyk <rdbyk@users.noreply.github.com>
2017-07-24 12:44:45 +0200

    reduce usage of std::endl in XMLNs.cpp

--- scilab/modules/xml/src/cpp/XMLNotHandledElement.cpp
+++ scilab/modules/xml/src/cpp/XMLNotHandledElement.cpp
@@ -3,6 +3,7 @@
  * Copyright (C) 2012 - Scilab Enterprises - Calixte DENIZET
  * Copyright (C) 2012 - 2016 - Scilab Enterprises
  * Copyright (C) 2017 - Dirk Reusch, Kybernetik Dr. Reusch
+ *
  * This file is hereby licensed under the terms of the GNU GPL v2.0,
  * pursuant to article 5.3.4 of the CeCILL v.2.1.
  * This file was originally licensed under the terms of the CeCILL v2.1,
