3a82de6c6c5aea0fd677255d38e20370ee41a16b
rdbyk <rdbyk@users.noreply.github.com>
2017-07-31 10:48:58 +0200

    saved wcslen call(s) in sci_convstr.cpp

--- scilab/modules/string/sci_gateway/cpp/sci_convstr.cpp
+++ scilab/modules/string/sci_gateway/cpp/sci_convstr.cpp
@@ -1,8 +1,8 @@
 /*
- *  Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
- *  Copyright (C) 2011-2011 - DIGITEO - Bruno JOFRET
- *
+ * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ * Copyright (C) 2011-2011 - DIGITEO - Bruno JOFRET
  * Copyright (C) 2012 - 2016 - Scilab Enterprises
+ * Copyright (C) 2017 - Dirk Reusch, Kybernetik Dr. Reusch
  *
  * This file is hereby licensed under the terms of the GNU GPL v2.0,
  * pursuant to article 5.3.4 of the CeCILL v.2.1.
@@ -84,7 +84,9 @@ types::Function::ReturnValue sci_convstr(types::typed_list &in, int _iRetCount, 
     for (int i = 0 ; i < pstConvertMe->getSize() ; ++i)
     {
         wchar_t *pwcsCurrent = pstConvertMe->get(i);
-        for (int j = 0 ; j < wcslen(pwcsCurrent) ; ++j)
+        size_t n = wcslen(pwcsCurrent);
+        
+        for (int j = 0 ; j < n ; ++j)
         {
             if (iConvertMode == 1)
             {
