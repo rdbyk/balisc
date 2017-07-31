bd6f2524da98cbdc28d3eb523a625e75e61590bd
rdbyk <rdbyk@users.noreply.github.com>
2017-07-31 10:52:40 +0200

    saved wcslen call(s) in sci_grep.cpp

--- scilab/modules/string/sci_gateway/cpp/sci_grep.cpp
+++ scilab/modules/string/sci_gateway/cpp/sci_grep.cpp
@@ -1,8 +1,8 @@
 /*
-* Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
-*  Copyright (C) 2010 - DIGITEO - Antoine ELIAS
-*
+ * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ * Copyright (C) 2010 - DIGITEO - Antoine ELIAS
  * Copyright (C) 2012 - 2016 - Scilab Enterprises
+ * Copyright (C) 2017 - Dirk Reusch, Kybernetik Dr. Reusch
  *
  * This file is hereby licensed under the terms of the GNU GPL v2.0,
  * pursuant to article 5.3.4 of the CeCILL v.2.1.
@@ -10,8 +10,8 @@
  * and continues to be available under such terms.
  * For more information, see the COPYING file which you should have received
  * along with this program.
-*
-*/
+ *
+ */
 
 /* desc : search position of a character string in an other string
 using regular express .                                         */
@@ -115,7 +115,7 @@ types::Function::ReturnValue sci_grep(types::typed_list &in, int _iRetCount, typ
 
     for (int i = 0 ; i < pS2->getSize() ; i++)
     {
-        if (wcslen(pS2->get(i)) == 0)
+        if ((pS2->get(i))[0] == L'\0')
         {
             Scierror(249, _("%s: Wrong values for input argument #%d: Non-empty strings expected.\n"), "grep", 2);
             return types::Function::Error;
