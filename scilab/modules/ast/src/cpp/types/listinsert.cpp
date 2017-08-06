ab1e122855feee45153d1c710bfe818dd92c89a2
rdbyk <rdbyk@users.noreply.github.com>
2017-08-06 13:27:22 +0200

    removed superfluous isDeletable in listinsert.cpp

--- scilab/modules/ast/src/cpp/types/listinsert.cpp
+++ scilab/modules/ast/src/cpp/types/listinsert.cpp
@@ -1,8 +1,8 @@
 /*
-*  Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
-*  Copyright (C) 2009-2009 - DIGITEO - Antoine ELIAS
-*
+ * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ * Copyright (C) 2009-2009 - DIGITEO - Antoine ELIAS
  * Copyright (C) 2012 - 2016 - Scilab Enterprises
+ * Copyright (C) 2017 - Dirk Reusch, Kybernetik Dr. Reusch
  *
  * This file is hereby licensed under the terms of the GNU GPL v2.0,
  * pursuant to article 5.3.4 of the CeCILL v.2.1.
@@ -33,11 +33,7 @@ ListInsert::~ListInsert()
     if (m_pInsert != NULL)
     {
         m_pInsert->DecreaseRef();
-        if (m_pInsert->isDeletable())
-        {
-            delete m_pInsert;
-            m_pInsert = NULL;
-        }
+        m_pInsert->killMe();
     }
 }
 
