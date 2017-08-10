d6f2cf2dc9beae517ef6cb27629cca3a7bcc56f8
rdbyk <rdbyk@users.noreply.github.com>
2017-08-10 11:21:50 +0200

    added new header balisc_elementary.h

---
+++ scilab/modules/elementary_functions/includes/balisc_elementary.h
@@ -0,0 +1,45 @@
+/* Balisc (https://github.com/rdbyk/balisc/)
+ * 
+ * Copyright (C) 2017 - Dirk Reusch, Kybernetik Dr. Reusch
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 
+ * 02110-1301, USA.
+ */
+ 
+#ifndef __BALISC_ELEMENTARY_H__
+#define __BALISC_ELEMENTARY_H__
+
+#include <sleef.h>
+
+#define balisc_acos_d Sleef_acos_u35
+#define balisc_asin_d Sleef_asin_u35
+#define balisc_copysign_d copysign
+#define balisc_fabs_d fabs
+#define balisc_log_d Sleef_log_u35
+#define balisc_sqrt_d sqrt
+
+#if defined(__SSE4_1__) || defined(__SSE4_2__)
+
+#define balisc_acos_m128d  Sleef_acosd2_u35sse4
+#define balisc_asin_m128d  Sleef_asind2_u35sse4
+
+#elif defined(__SSE2__)
+
+#define balisc_acos_m128d  Sleef_acosd2_u35sse2
+#define balisc_asin_m128d  Sleef_asind2_u35sse2
+
+#endif
+
+#endif /* __BALISC_ELEMENTARY_H__ */
