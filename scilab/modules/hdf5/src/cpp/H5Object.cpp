3b34dd827b2191972b8b078185025e3ef62e8dd5
rdbyk <rdbyk@users.noreply.github.com>
2017-08-12 13:18:06 +0200

    use pre increment for iterator(s) in H5Object.cpp

--- scilab/modules/hdf5/src/cpp/H5Object.cpp
+++ scilab/modules/hdf5/src/cpp/H5Object.cpp
@@ -1,8 +1,8 @@
 /*
  * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
  * Copyright (C) 2012 - Scilab Enterprises - Calixte DENIZET
- *
  * Copyright (C) 2012 - 2016 - Scilab Enterprises
+ * Copyright (C) 2017 - Dirk Reusch, Kybernetik Dr. Reusch
  *
  * This file is hereby licensed under the terms of the GNU GPL v2.0,
  * pursuant to article 5.3.4 of the CeCILL v.2.1.
@@ -44,7 +44,7 @@ H5Object::~H5Object()
 void H5Object::cleanup()
 {
     locked = true;
-    for (std::set<H5Object *>::iterator it = children.begin(); it != children.end(); it++)
+    for (std::set<H5Object *>::iterator it = children.begin(); it != children.end(); ++it)
     {
         delete *it;
     }
