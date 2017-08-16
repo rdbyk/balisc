0f3fd2e6e1bd8fa724341c82b05ac02f25c099ef
rdbyk <rdbyk@users.noreply.github.com>
2017-08-16 20:12:12 +0200

    remove unused operations_tools.{h,c}

--- scilab/modules/ast/src/c/operations/matrix_addition.c
+++ scilab/modules/ast/src/c/operations/matrix_addition.c
@@ -1,8 +1,8 @@
 /*
-*  Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
-*  Copyright (C) 2009 - DIGITEO - Antoine ELIAS
-*
+ * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ * Copyright (C) 2009 - DIGITEO - Antoine ELIAS
  * Copyright (C) 2012 - 2016 - Scilab Enterprises
+ * Copyright (C) 2017 - Dirk Reusch, Kybernetik Dr. Reusch
  *
  * This file is hereby licensed under the terms of the GNU GPL v2.0,
  * pursuant to article 5.3.4 of the CeCILL v.2.1.
@@ -10,14 +10,13 @@
  * and continues to be available under such terms.
  * For more information, see the COPYING file which you should have received
  * along with this program.
-*
-*/
+ *
+ */
 
 #include <stdio.h>
 #include <string.h>
 #include "matrix_addition.h"
 #include "core_math.h"
-#include "operations_tools.h"
 #include "elem_common.h"
 
 /* SinglePoly + SinglePoly*/
