c5c839bd0b223e0c7c75acd445e7c314f26539ba
rdbyk <rdbyk@users.noreply.github.com>
2017-08-20 15:09:16 +0200

    removed include finite.h in implicitlist.cpp

--- scilab/modules/ast/src/cpp/types/implicitlist.cpp
+++ scilab/modules/ast/src/cpp/types/implicitlist.cpp
@@ -1,8 +1,8 @@
 /*
-*  Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
-*  Copyright (C) 2008-2008 - DIGITEO - Antoine ELIAS
-*
+ * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ * Copyright (C) 2008-2008 - DIGITEO - Antoine ELIAS
  * Copyright (C) 2012 - 2016 - Scilab Enterprises
+ * Copyrigth (C) 2017 - Dirk Reusch, Kybernetik Dr. Reusch
  *
  * This file is hereby licensed under the terms of the GNU GPL v2.0,
  * pursuant to article 5.3.4 of the CeCILL v.2.1.
@@ -10,8 +10,9 @@
  * and continues to be available under such terms.
  * For more information, see the COPYING file which you should have received
  * along with this program.
-*
-*/
+ *
+ */
+
 #include <sstream>
 #include <math.h>
 #include "double.hxx"
@@ -35,7 +36,6 @@ extern "C"
 {
 #include "core_math.h"
 #include "elem_common.h"
-#include "finite.h"
 }
 
 std::wstring printInLinePoly(types::SinglePoly* _pPoly, std::wstring _stVar);
