8ad10afd388c2bf99bff9fda0b7082d7d10b58a0
rdbyk <rdbyk@users.noreply.github.com>
2017-08-27 16:49:07 +0200

    joined two for loops in sci_find.cpp

--- scilab/modules/boolean/sci_gateway/cpp/sci_find.cpp
+++ scilab/modules/boolean/sci_gateway/cpp/sci_find.cpp
@@ -1,8 +1,8 @@
 /*
-* Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
-* Copyright (C) 2012 - DIGITEO - Antoine ELIAS
-*
+ * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ * Copyright (C) 2012 - DIGITEO - Antoine ELIAS
  * Copyright (C) 2012 - 2016 - Scilab Enterprises
+ * Copyright (C) 2017 - Dirk Reusch, Kybernetik Dr. Reusch
  *
  * This file is hereby licensed under the terms of the GNU GPL v2.0,
  * pursuant to article 5.3.4 of the CeCILL v.2.1.
@@ -10,8 +10,8 @@
  * and continues to be available under such terms.
  * For more information, see the COPYING file which you should have received
  * along with this program.
-*
-*/
+ *
+ */
 /*--------------------------------------------------------------------------*/
 
 #include "boolean_gw.hxx"
@@ -213,10 +213,6 @@ types::Function::ReturnValue sci_find(types::typed_list &in, int _iRetCount, typ
         for (int i = 0 ; i < iValues ; i++)
         {
             piCoord[i] = new int[_iRetCount];
-        }
-
-        for (int i = 0 ; i < iValues ; i++)
-        {
             getCoordFromIndex(piIndex[i], piCoord[i], piDims, iDims);
         }
 
