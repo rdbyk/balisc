89b899c1d59387f5c7e8229785c5b10de9e203e2
rdbyk <rdbyk@users.noreply.github.com>
2017-08-28 13:17:35 +0200

    removed unused transposeMatrixInt

--- scilab/modules/core/includes/transposeMatrix.h
+++ scilab/modules/core/includes/transposeMatrix.h
@@ -1,18 +1,18 @@
 /*
-* Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
-* Copyright (C) 2009 - DIGITEO - Allan CORNET
-*
+ * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ * Copyright (C) 2009 - DIGITEO - Allan CORNET
  * Copyright (C) 2012 - 2016 - Scilab Enterprises
- *
+ * Copyright (C) 2017 - Dirk Reusch, Kybernetik Dr. Reusch
+ * 
  * This file is hereby licensed under the terms of the GNU GPL v2.0,
  * pursuant to article 5.3.4 of the CeCILL v.2.1.
  * This file was originally licensed under the terms of the CeCILL v2.1,
  * and continues to be available under such terms.
  * For more information, see the COPYING file which you should have received
  * along with this program.
-*
-*/
-/*--------------------------------------------------------------------------*/
+ *
+ */
+
 #ifndef __TRANSPOSEMATRIX_H__
 #define __TRANSPOSEMATRIX_H__
 
@@ -25,23 +25,4 @@
 */
 double *transposeMatrixDouble(int C, int L, double *MatrixDouble);
 
-/**
-* transpose a (int*)
-* param[in] C
-* param[in] L
-* param[in] MatrixInt
-* return transposed matrix
-*/
-int *transposeMatrixInt(int C, int L, int *MatrixInt);
-
-/**
-* transpose a (char **)
-* param[in] C
-* param[in] L
-* param[in] MatrixStr
-* return transposed matrix
-*/
-//char **transposeMatrixStrings(int C,int L,char **MatrixStr);
-
 #endif /* __TRANSPOSEMATRIX_H__ */
-/*--------------------------------------------------------------------------*/
