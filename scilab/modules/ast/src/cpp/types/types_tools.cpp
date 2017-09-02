cbb05b259e4276e3dba917e48e092c2e39b233ef
rdbyk <rdbyk@users.noreply.github.com>
2017-09-02 17:06:43 +0200

    simplified if-else logic in checkIndexesArguments

--- scilab/modules/ast/src/cpp/types/types_tools.cpp
+++ scilab/modules/ast/src/cpp/types/types_tools.cpp
@@ -2,6 +2,7 @@
  * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
  * Copyright (C) 2011 - DIGITEO - Antoine ELIAS
  * Copyright (C) 2012 - 2016 - Scilab Enterprises
+ * Copyrigth (C) 2017 - Dirk Reusch, Kybernetik Dr. Reusch
  *
  * This file is hereby licensed under the terms of the GNU GPL v2.0,
  * pursuant to article 5.3.4 of the CeCILL v.2.1.
@@ -502,8 +503,7 @@ int checkIndexesArguments(InternalType* _pRef, typed_list* _pArgsIn, typed_list*
                 pCurrentArg->killMe();
                 pCurrentArg = NULL;
             }
-
-            if (pCurrentArg)
+            else
             {
                 int size = pCurrentArg->getSize();
                 double* dbl = pCurrentArg->get();
