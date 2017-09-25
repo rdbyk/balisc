d46b498ec7b64c32e9cc43399771247ed9b22c35
rdbyk <rdbyk@users.noreply.github.com>
2017-09-25 17:58:34 +0200

    fixes due to modified resize in arrayof.cpp

--- scilab/modules/ast/src/cpp/types/arrayof.cpp
+++ scilab/modules/ast/src/cpp/types/arrayof.cpp
@@ -338,15 +338,7 @@ ArrayOf<T>* ArrayOf<T>::insert(typed_list* _pArgs, InternalType* _pSource)
     //before resize, check input dimension
     if (bNeedToResize)
     {
-        ArrayOf<T>* pTemp = resize(piNewDims, iNewDims);
-        if (pTemp == NULL)
-        {
-            delete[] piCountDim;
-            delete[] piMaxDim;
-            //free pArg content
-            cleanIndexesArguments(_pArgs, &pArg);
-            return NULL;
-        }
+        resize(piNewDims, iNewDims);
     }
     else
     {
