a318d97b4659eacbeaeeaa0356e8dc974a1f3342
rdbyk <rdbyk@users.noreply.github.com>
2017-09-26 12:25:30 +0200

    fix for bool return type of set in api_boolean.hpp

--- scilab/modules/api_scilab/src/cpp/template/api_boolean.hpp
+++ scilab/modules/api_scilab/src/cpp/template/api_boolean.hpp
@@ -1,16 +1,16 @@
 /*
-* Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
-* Copyright (C) 2015 - Scilab Enterprises - Antoine ELIAS
-*
+ * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ * Copyright (C) 2015 - Scilab Enterprises - Antoine ELIAS
  * Copyright (C) 2012 - 2016 - Scilab Enterprises
- *
+ * Copyright (C) 2017 - Dirk Reusch, Kybernetik Dr. Reusch
+ * 
  * This file is hereby licensed under the terms of the GNU GPL v2.0,
  * pursuant to article 5.3.4 of the CeCILL v.2.1.
  * This file was originally licensed under the terms of the CeCILL v2.1,
  * and continues to be available under such terms.
  * For more information, see the COPYING file which you should have received
  * along with this program.
-*/
+ */
 
 #include "gatewaystruct.hxx"
 #include "bool.hxx"
@@ -131,7 +131,7 @@ scilabStatus API_PROTO(setBooleanArray)(scilabEnv env, scilabVar var, const int*
     }
 #endif
 
-    bool bset = b->set(vals) != nullptr;
+    bool bset = b->set(vals);
 #ifdef __API_SCILAB_SAFE__
     if (bset == false)
     {
