eb12008ffe1106e664df2caefe0e7f277770b1a3
rdbyk <rdbyk@users.noreply.github.com>
2017-10-08 17:59:44 +0200

    use set_ in ArrayOf<T>::append

--- scilab/modules/ast/src/cpp/types/arrayof.cpp
+++ scilab/modules/ast/src/cpp/types/arrayof.cpp
@@ -759,7 +759,7 @@ void ArrayOf<T>::append(int _iRows, int _iCols, InternalType* _poSource)
             for (int j = 0; j < iCols; j++)
             {
                 int c = _iCols + j;
-                set(r, c, pGT->get(i, j));
+                set_(r, c, pGT->get(i, j));
                 setImg(r, c, pGT->getImg(i, j));
             }
         }
@@ -771,7 +771,7 @@ void ArrayOf<T>::append(int _iRows, int _iCols, InternalType* _poSource)
             int r = _iRows + i;
             for (int j = 0; j < iCols; j++)
             {
-                set(r, _iCols + j, pGT->get(i, j));
+                set_(r, _iCols + j, pGT->get(i, j));
             }
         }
     }
