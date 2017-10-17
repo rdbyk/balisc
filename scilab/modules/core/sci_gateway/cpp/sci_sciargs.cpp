808cea0ca0b00b91f90a0177c5c73d96b61334c2
rdbyk <rdbyk@users.noreply.github.com>
2017-10-17 13:52:39 +0200

    use set_ in sci_sciargs.cpp

--- scilab/modules/core/sci_gateway/cpp/sci_sciargs.cpp
+++ scilab/modules/core/sci_gateway/cpp/sci_sciargs.cpp
@@ -2,8 +2,8 @@
  * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
  * Copyright (C) 2006 - INRIA Allan CORNET
  * Copyright (C) 2011 - DIGITEO - Antoine ELIAS
- *
  * Copyright (C) 2012 - 2016 - Scilab Enterprises
+ * Copyright (C) 2017 - Dirk Reusch, Kybernetik Dr. Reusch
  *
  * This file is hereby licensed under the terms of the GNU GPL v2.0,
  * pursuant to article 5.3.4 of the CeCILL v.2.1.
@@ -47,7 +47,7 @@ types::Function::ReturnValue sci_sciargs(types::typed_list &in, int _iRetCount, 
     }
 
     types::String* pS = new types::String(iCount, 1);
-    pS->set(pwstCmdLineArgs);
+    pS->set_(pwstCmdLineArgs);
     out.push_back(pS);
     freeArrayOfWideString(pwstCmdLineArgs, iCount);
     return types::Function::OK;
