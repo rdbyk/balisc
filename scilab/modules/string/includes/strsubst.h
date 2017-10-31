a977ec9858610f1fc75e8dd0f9d97122d2481511
rdbyk <rdbyk@users.noreply.github.com>
2017-10-31 10:48:00 +0100

    removed unused code from strsubst.c

--- scilab/modules/string/includes/strsubst.h
+++ scilab/modules/string/includes/strsubst.h
@@ -1,9 +1,8 @@
-
 /*
  * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
  * Copyright (C) INRIA - Allan CORNET
- *
  * Copyright (C) 2012 - 2016 - Scilab Enterprises
+ * Copyrigth (C) 2017 - Dirk Reusch, Kybernetik Dr. Reusch
  *
  * This file is hereby licensed under the terms of the GNU GPL v2.0,
  * pursuant to article 5.3.4 of the CeCILL v.2.1.
@@ -22,17 +21,6 @@
 #include "dynlib_string.h"
 
 /**
-* substitute a character string by another in a character string in string matrix
-* @param[in] strings_input
-* @param[in] dimension of strings_input
-* @param[in] string_to_search
-* @param[in] replacement_string
-* @return substituted string matrix
-*/
-STRING_IMPEXP char **strsubst(const char **strings_input, int strings_dim, const char *string_to_search, const char *replacement_string);
-STRING_IMPEXP char **strsubst_reg(const char **strings_input, int strings_dim, const char *string_to_search, const char *replacement_string, int *ierr);
-
-/**
 * substitute a character string by another in a character string
 * @param[in] string
 * @param[in] string to search in input_string
@@ -41,8 +29,6 @@ STRING_IMPEXP char **strsubst_reg(const char **strings_input, int strings_dim, c
 * @return substituted string
 */
 STRING_IMPEXP char *strsub(const char* input_string, const char* string_to_search, const char* replacement_string);
-STRING_IMPEXP char *strsub_reg(const char* input_string, const char* string_to_search, const char* replacement_string, int *ierr);
-
 
 STRING_IMPEXP wchar_t **wcssubst(const wchar_t** _pwstInput, int _iInputSize, const wchar_t* _pwstSearch, const wchar_t* _pwstReplace);
 STRING_IMPEXP wchar_t **wcssubst_reg(const wchar_t** _pwstInput, int _iInputSize, const wchar_t* _pwstSearch, const wchar_t* _pwstReplace, int* _piErr);
