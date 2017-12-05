9734d2f8feecd17aa58315bd1bfefa4e99846302
rdbyk <rdbyk@users.noreply.github.com>
2017-12-05 19:31:08 +0100

    fixed return value for empty list in sci_librarieslist

--- scilab/modules/functions/sci_gateway/cpp/sci_librarieslist.cpp
+++ scilab/modules/functions/sci_gateway/cpp/sci_librarieslist.cpp
@@ -1,8 +1,8 @@
 /*
-* Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
-* Copyright (C) 2015 - Scilab Enterprises - Antoine ELIAS
-*
+ * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ * Copyright (C) 2015 - Scilab Enterprises - Antoine ELIAS
  * Copyright (C) 2012 - 2016 - Scilab Enterprises
+ * Copyrigth (C) 2017 - Dirk Reusch, Kybernetik Dr. Reusch
  *
  * This file is hereby licensed under the terms of the GNU GPL v2.0,
  * pursuant to article 5.3.4 of the CeCILL v.2.1.
@@ -10,14 +10,15 @@
  * and continues to be available under such terms.
  * For more information, see the COPYING file which you should have received
  * along with this program.
-*
-*/
+ *
+ */
 
 #include <string.h>
 #include "funcmanager.hxx"
 #include "context.hxx"
 #include "functions_gw.hxx"
 #include "string.hxx"
+#include "double.hxx"
 
 extern "C"
 {
@@ -37,14 +38,23 @@ types::Function::ReturnValue sci_librarieslist(types::typed_list &in, int _iRetC
     std::list<std::wstring> lst;
     int size = symbol::Context::getInstance()->getLibrariesList(lst);
 
-    types::String* pOut = new types::String(size, 1);
-    int i = 0;
-    for (auto l : lst)
+    if (size)
     {
-        pOut->set(i++, l.c_str());
+        types::String* pOut = new types::String(size, 1);
+
+        int i = 0;
+        for (auto l : lst)
+        {
+            pOut->set(i++, l.c_str());
+        }
+
+        out.push_back(pOut);
+    }
+    else
+    {
+        out.push_back(types::Double::Empty());
     }
 
-    out.push_back(pOut);
     return types::Function::OK;
 }
 /*--------------------------------------------------------------------------*/
