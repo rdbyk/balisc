4c781132a3c54df13f6d6ff3617f8bb7a4a437f9
rdbyk <rdbyk@users.noreply.github.com>
2017-12-07 20:47:18 +0100

    removed unused header from InitializePreferences.c

--- scilab/modules/core/src/c/InitializePreferences.c
+++ scilab/modules/core/src/c/InitializePreferences.c
@@ -1,8 +1,8 @@
 /*
  * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
  * Copyright (C) 2012 - Scilab Enterprises - Calixte DENIZET
- *
  * Copyright (C) 2012 - 2016 - Scilab Enterprises
+ * Copyright (C) 2017 - Dirk Reusch, Kybernetik Dr. Reusch
  *
  * This file is hereby licensed under the terms of the GNU GPL v2.0,
  * pursuant to article 5.3.4 of the CeCILL v.2.1.
@@ -18,7 +18,6 @@
 #include "getScilabPreference.h"
 #include "api_scilab.h"
 #include "configvariable_interface.h"
-#include "setlines.h"
 #include "setformat.h"
 #include "os_string.h"
 #include "TerminateHistoryManager.h"
