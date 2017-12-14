c505968d12f2a74b821c232698b8781b14df9d4b
rdbyk <rdbyk@users.noreply.github.com>
2017-12-14 15:09:15 +0100

    removed superfluous include in sci_getdebuginfo.c

--- scilab/modules/core/sci_gateway/c/sci_getdebuginfo.c
+++ scilab/modules/core/sci_gateway/c/sci_getdebuginfo.c
@@ -1,8 +1,8 @@
 /*
  * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
  * Copyright (C) 2008 - INRIA - Sylvestre LEDRU
- *
  * Copyright (C) 2012 - 2016 - Scilab Enterprises
+ * Copyright (C) 2017 - Dirk Reusch, Kybernetik Dr. Reusch
  *
  * This file is hereby licensed under the terms of the GNU GPL v2.0,
  * pursuant to article 5.3.4 of the CeCILL v.2.1.
@@ -14,7 +14,6 @@
  */
 #include <string.h>
 #include <stdio.h>
-#include "gw_core.h"
 #include "api_scilab.h"
 #include "version.h"
 #include "sci_malloc.h"
