f834764222abe2e4dfad7b9ba017c9ddc3e4e7cc
rdbyk <rdbyk@users.noreply.github.com>
2018-01-04 18:17:04 +0100

    removed useless include from sci_genlib.cpp

--- scilab/modules/io/sci_gateway/cpp/sci_genlib.cpp
+++ scilab/modules/io/sci_gateway/cpp/sci_genlib.cpp
@@ -2,7 +2,7 @@
  * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
  * Copyright (C) 2006 - INRIA - Allan CORNET
  * Copyright (C) 2012 - 2016 - Scilab Enterprises
- * Copyright (C) 2017 - Dirk Reusch, Kybernetik Dr. Reusch
+ * Copyright (C) 2017 - 2018 Dirk Reusch, Kybernetik Dr. Reusch
  *
  * This file is hereby licensed under the terms of the GNU GPL v2.0,
  * pursuant to article 5.3.4 of the CeCILL v.2.1.
@@ -44,7 +44,6 @@ extern "C"
 #include "FileExist.h"
 #include "deleteafile.h"
 #include "os_string.h"
-#include "splitpath.h"
 #include "os_wfopen.h"
 #include "sciprint.h"
 #include "freeArrayOfString.h"
