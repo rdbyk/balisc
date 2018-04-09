ac4337395487e1f69fc44aee33520c5815940a63
rdbyk <rdbyk@users.noreply.github.com>
2018-04-09 13:00:42 +0200

    removed getCountLines from File class

--- scilab/modules/ast/src/cpp/types/file.cpp
+++ scilab/modules/ast/src/cpp/types/file.cpp
@@ -2,7 +2,7 @@
  * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
  * Copyright (C) 2010-2010 - DIGITEO - Antoine ELIAS
  * Copyright (C) 2012 - 2016 - Scilab Enterprises
- * Copyright (C) 2017 - Dirk Reusch, Kybernetik Dr. Reusch
+ * Copyright (C) 2017 - 2018 Dirk Reusch, Kybernetik Dr. Reusch
  *
  * This file is hereby licensed under the terms of the GNU GPL v2.0,
  * pursuant to article 5.3.4 of the CeCILL v.2.1.
@@ -183,23 +183,6 @@ void File::setFilename(const std::wstring& _stFilename)
 std::wstring& File::getFilename()
 {
     return m_stFilename;
-}
-
-int File::getCountLines()
-{
-    char* pstFileName = wide_string_to_UTF8(m_stFilename.c_str());
-    std::ifstream in(pstFileName);
-    std::string stLine;
-    int iLines = 0;
-
-    while (std::getline(in, stLine))
-    {
-        iLines++;
-    }
-
-    in.close();
-    FREE(pstFileName);
-    return iLines;
 }
 
 }
