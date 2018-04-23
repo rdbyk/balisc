62ee7fb210ce09b38125bc10fb4ab0b3b8278888
rdbyk <rdbyk@users.noreply.github.com>
2018-04-23 13:55:50 +0200

    fixed {ListDelete,ListInsert}::toString

--- scilab/modules/ast/includes/types/listdelete.hxx
+++ scilab/modules/ast/includes/types/listdelete.hxx
@@ -2,7 +2,7 @@
  * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
  * Copyright (C) 2009-2010 - DIGITEO - Antoine ELIAS
  * Copyright (C) 2012 - 2016 - Scilab Enterprises
- * Copyright (C) 2017 - Dirk Reusch, Kybernetik Dr. Reusch
+ * Copyright (C) 2017 - 2018 Dirk Reusch, Kybernetik Dr. Reusch
  *
  * This file is hereby licensed under the terms of the GNU GPL v2.0,
  * pursuant to article 5.3.4 of the CeCILL v.2.1.
@@ -45,8 +45,6 @@ public :
     }
 
     void                    whoAmI();
-
-    bool                    toString(std::wostringstream& ostr);
 
     /* return type as string ( double, int, cell, list, ... )*/
     virtual std::wstring    getTypeStr() const
