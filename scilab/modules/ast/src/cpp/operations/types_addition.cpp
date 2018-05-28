bb019b7307965667c927a21d8decbcc8c9129a17
rdbyk <rdbyk@users.noreply.github.com>
2018-05-28 11:54:44 +0200

    reuse left input arg in matrix+scalar string op

--- scilab/modules/ast/src/cpp/operations/types_addition.cpp
+++ scilab/modules/ast/src/cpp/operations/types_addition.cpp
@@ -1723,25 +1723,44 @@ InternalType* add_S_M<String, String, String>(String* _pL, String* _pR)
 template<>
 InternalType* add_M_S<String, String, String>(String* _pL, String* _pR)
 {
-    String* pOut = new String(_pL->getDims(), _pL->getDimsArray());
     int size = _pL->getSize();
-    int* sizeOut = new int[size];
     wchar_t* pwstR = _pR->getFirst();
     int sizeR = (int)wcslen(pwstR);
 
-    for (int i = 0 ; i < size ; ++i)
+    String* pOut;
+    if (_pL->getRef() > 0)
     {
-        wchar_t* pwstL = _pL->get(i);
-        int sizeL = (int)wcslen(pwstL);
+        int* sizeOut = new int[size];
 
-        sizeOut[i] = sizeL + sizeR + 1;
-        wchar_t* pwstOut = (wchar_t*) MALLOC(sizeOut[i] * sizeof(wchar_t));
-        //assign ptr without strdup
-        pOut->get()[i] = pwstOut;
+        pOut = new String(_pL->getDims(), _pL->getDimsArray());
+
+        for (int i = 0 ; i < size ; ++i)
+        {
+            wchar_t* pwstL = _pL->get(i);
+            int sizeL = (int)wcslen(pwstL);
+
+            sizeOut[i] = sizeL + sizeR + 1;
+            wchar_t* pwstOut = (wchar_t*)MALLOC(sizeOut[i] * sizeof(wchar_t));
+            //assign ptr without strdup
+            pOut->get()[i] = pwstOut;
+        }
+
+        add(_pL->get(), size, pwstR, sizeOut, pOut->get());
+        delete[] sizeOut;
+    }
+    else
+    {
+        pOut = _pL;
+
+        for (int i = 0; i < size; ++i)
+        {
+            wchar_t* pwstL = pOut->get(i);
+            int sizeL = (int)wcslen(pwstL);
+            pOut->get()[i] = (wchar_t*)REALLOC(pwstL, (sizeL + sizeR + 1) * sizeof(wchar_t));
+            wcscat(pOut->get(i), pwstR);
+        }
     }
 
-    add(_pL->get(), size, pwstR, sizeOut, pOut->get());
-    delete[] sizeOut;
     return pOut;
 }
 
