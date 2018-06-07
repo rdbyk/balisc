e411faf0257a08508c3d3a4db0ed88aece1a6662
rdbyk <rdbyk@users.noreply.github.com>
2018-06-07 10:47:03 +0200

    correct printing of func decl wo/ return args in printvisitor.cpp

--- scilab/modules/ast/src/cpp/ast/printvisitor.cpp
+++ scilab/modules/ast/src/cpp/ast/printvisitor.cpp
@@ -2,7 +2,7 @@
  * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
  * Copyright (C) 2008-2008 - DIGITEO - Bruno JOFRET
  * Copyright (C) 2012 - 2016 - Scilab Enterprises
- * Copyright (C) 2017 - Dirk Reusch, Kybernetik Dr. Reusch
+ * Copyright (C) 2017 - 2018 Dirk Reusch, Kybernetik Dr. Reusch
  *
  * This file is hereby licensed under the terms of the GNU GPL v2.0,
  * pursuant to article 5.3.4 of the CeCILL v.2.1.
@@ -1051,10 +1051,9 @@ void PrintVisitor::visit (const FunctionDec  &e)
         *ostr << SCI_CLOSE_RETURNS;
     }
 
-    *ostr << " ";
     if (e.getReturns().getAs<ArrayListVar>()->getVars().size() > 0)
     {
-        *ostr << SCI_ASSIGN << " ";
+        *ostr << " " << SCI_ASSIGN << " ";
     }
 
     // Then get the function name
