5689399aaa517c83fce88706ef021197de160e79
rdbyk <rdbyk@users.noreply.github.com>
2018-07-16 16:09:44 +0200

    simplfified {new,delete}Matrix in sciMatrix.{c,h}

--- scilab/modules/graphics/src/c/sciMatrix.c
+++ scilab/modules/graphics/src/c/sciMatrix.c
@@ -2,7 +2,7 @@
  * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
  * Copyright (C) 2006 - INRIA - Jean-Baptiste Silvy
  * Copyright (C) 2012 - 2016 - Scilab Enterprises
- * Copyright (C) 2017 - Dirk Reusch, Kybernetik Dr. Reusch
+ * Copyright (C) 2017 - 2018 Dirk Reusch, Kybernetik Dr. Reusch
  *
  * This file is hereby licensed under the terms of the GNU GPL v2.0,
  * pursuant to article 5.3.4 of the CeCILL v.2.1.
@@ -24,56 +24,36 @@
 #include "sciMatrix.h"
 #include "sci_malloc.h"
 
-/*----------------------------------------------------------------------------------*/
-sciMatrix * emptyMatrix(void)
+sciMatrix* newMatrix(int nbRow, int nbCol)
 {
-    sciMatrix * newMat;
-    newMat = MALLOC(sizeof(sciMatrix));
+    int i = 0;
+
+    /* create the matrix */
+    sciMatrix* newMat = MALLOC(sizeof(sciMatrix));
+
     if (newMat == NULL)
     {
         return NULL ;
     }
-    newMat->data  = NULL;
-    newMat->nbCol = 0   ;
-    newMat->nbRow = 0   ;
-
-    return newMat;
-}
-/*----------------------------------------------------------------------------------*/
-sciMatrix * newMatrix(int nbRow, int nbCol)
-{
-    int i = 0;
-    /* create the matrix */
-    sciMatrix * newMat = emptyMatrix();
 
     /* allocate the data */
-    newMat->data  = MALLOC((nbRow * nbCol) * sizeof(void *));
+    newMat->data  = CALLOC((nbRow * nbCol), sizeof(void*));
     newMat->nbRow = nbRow;
     newMat->nbCol = nbCol;
 
-    /* initialize to NULL */
-    for (i = 0 ; i < nbRow * nbCol ; i++)
-    {
-        newMat->data[i] = NULL;
-    }
-
     return newMat;
 }
-/*----------------------------------------------------------------------------------*/
+
 void deleteMatrix(sciMatrix * mat)
 {
     int i = 0;
-    for (i = 0 ; i < mat->nbRow * mat->nbCol ; i++)
+
+    for (i = 0; i < mat->nbRow * mat->nbCol; ++i)
     {
         FREE(mat->data[i]);
-        mat->data[i] = NULL;
     }
+
     FREE(mat->data);
-    mat->data = NULL;
-
-    mat->nbCol = 0;
-    mat->nbRow = 0;
-
     FREE(mat);
+    mat = NULL;
 }
-/*----------------------------------------------------------------------------------*/
