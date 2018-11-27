scilab/scilab

* Bug 8143 fixed: mtlb_qr had wrong input checking
  http://bugzilla.scilab.org/8143

  Just unblocking the simple usage.
  Second input still possible, even if it is unused.
  Matlab syntaxes accept additional inputs.
  Converter to be overhauled.

Change-Id: Ie684a68db563b701984ee5d84760d933a77eea55

scilab/modules/compatibility_functions/macros/mtlb_qr.sci
@@ -10,16 +10,18 @@
 // For more information, see the COPYING file which you should have received
 // along with this program.
 
-function [Q,R,E]=mtlb_qr(A,B)
+function [Q,R,E] = mtlb_qr(A,B)
     // Emulation function for qr() Matlab function
 
     [lhs,rhs]=argn()
 
-    if lhs<>3 then
-        error(msprintf(gettext("%s: Wrong number of output argument(s): %d expected.\n"),"mtlb_qr",3));
+    if and(rhs<>[1 2]) then
+        msg = gettext("%s: Wrong number of input arguments: %d or %d expected.\n")
+        error(msprintf(msg,"mtlb_qr",1,2));
     end
-    if rhs<>2 then
-        error(msprintf(gettext("%s: Wrong number of input argument(s): %d expected.\n"),"mtlb_qr",2));
+    if lhs > 3 then
+        msg = gettext("%s: Wrong number of output arguments: %d to %d expected.\n")
+        error(msprintf(msg, "mtlb_qr", 1, 3));
     end
 
     [Q,R,E] = qr(A)

rdbyk/balisc

(cherry picked from commit 896458c4a9416677e8c32defcb771caeff1cef45)
Conflicts:
	scilab/modules/compatibility_functions/macros/mtlb_qr.sci

scilab/modules/compatibility_functions/macros/mtlb_qr.sci
@@ -10,14 +10,17 @@
 // For more information, see the COPYING file which you should have received
 // along with this program.
 
-function [Q,R,E]=mtlb_qr(A,B)
+function [Q,R,E] = mtlb_qr(A,B)
     // Emulation function for qr() Matlab function
 
-    if nargout<>3 then
-        error(msprintf(gettext("%s: Wrong number of output arguments: %d expected.\n"),"mtlb_qr",3));
+    if nargin <> [1 2] then
+        msg = gettext("%s: Wrong number of input arguments: %d or %d expected.\n")
+        error(msprintf(msg,"mtlb_qr",1,2));
     end
-    if nargin<>2 then
-        error(msprintf(gettext("%s: Wrong number of input arguments: %d expected.\n"),"mtlb_qr",2));
+
+    if nargout > 3 then
+        msg = gettext("%s: Wrong number of output arguments: %d to %d expected.\n")
+        error(msprintf(msg, "mtlb_qr", 1, 3));
     end
 
     [Q,R,E] = qr(A)