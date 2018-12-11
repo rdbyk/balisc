3df7bcd92f3e70f50ccbb12baae2fd3a869e6919
rdbyk <rdbyk@users.noreply.github.com>
2018-12-11 15:26:15 +0100

    remove {get,split}URL functions

--- scilab/modules/fileio/includes/gw_fileio.h
+++ scilab/modules/fileio/includes/gw_fileio.h
@@ -2,7 +2,7 @@
  * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
  * Copyright (C) 2006 - INRIA - Allan CORNET
  * Copyright (C) 2012 - 2016 - Scilab Enterprises
- * Copyright (C) 2017 - Dirk Reusch, Kybernetik Dr. Reusch
+ * Copyright (C) 2017 - 2018 Dirk Reusch, Kybernetik Dr. Reusch
  *
  * This file is hereby licensed under the terms of the GNU GPL v2.0,
  * pursuant to article 5.3.4 of the CeCILL v.2.1.
@@ -24,8 +24,6 @@ STACK_GATEWAY_PROTOTYPE(sci_copyfile);
 STACK_GATEWAY_PROTOTYPE(sci_movefile);
 STACK_GATEWAY_PROTOTYPE(sci_fscanfMat);
 STACK_GATEWAY_PROTOTYPE(sci_fprintfMat);
-STACK_GATEWAY_PROTOTYPE(sci_getURL);
-STACK_GATEWAY_PROTOTYPE(sci_splitURL);
 STACK_GATEWAY_PROTOTYPE(sci_tempname);
 
 #endif /* __GW_FILEIO_H__ */
