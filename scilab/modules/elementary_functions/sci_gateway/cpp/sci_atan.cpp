4fe2ada3e69bb48398143f83286629664ec10ced
rdbyk <rdbyk@users.noreply.github.com>
2019-04-16 16:34:54 +0200

    fixed an error message of atan

--- scilab/modules/elementary_functions/sci_gateway/cpp/sci_atan.cpp
+++ scilab/modules/elementary_functions/sci_gateway/cpp/sci_atan.cpp
@@ -2,7 +2,7 @@
  * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
  * Copyright (C) 2012 - DIGITEO - Cedric DELAMARRE
  * Copyright (C) 2012 - 2016 - Scilab Enterprises
- * Copyright (C) 2017 - 2018 Dirk Reusch, Kybernetik Dr. Reusch
+ * Copyright (C) 2017 - 2019 Dirk Reusch, Kybernetik Dr. Reusch
  *
  * This file is hereby licensed under the terms of the GNU GPL v2.0,
  * pursuant to article 5.3.4 of the CeCILL v.2.1.
@@ -39,7 +39,7 @@ types::Function::ReturnValue sci_atan(types::typed_list &in, int _iRetCount, typ
 
     if (in.size() < 1 || in.size() > 2)
     {
-        Scierror(72, fname, 1, 2);
+        Scierror(72, 1, 2);
         return types::Function::Error;
     }
 
