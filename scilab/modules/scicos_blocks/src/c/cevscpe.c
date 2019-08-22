07a945684fe8d041ef2d2d72d250fc79ca27cd3e
rdbyk <rdbyk@users.noreply.github.com>
2019-08-22 13:02:25 +0200

    removed useless includes of "elementary_functions.h"

--- scilab/modules/scicos_blocks/src/c/cevscpe.c
+++ scilab/modules/scicos_blocks/src/c/cevscpe.c
@@ -1,8 +1,8 @@
 /*
- *  Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
- *  Copyright (C) 2011 - Scilab Enterprises - Clement DAVID
- *
+ * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ * Copyright (C) 2011 - Scilab Enterprises - Clement DAVID
  * Copyright (C) 2012 - 2016 - Scilab Enterprises
+ * Copyright (C) 2019 - Dirk Reusch, Kybernetik Dr. Reusch
  *
  * This file is hereby licensed under the terms of the GNU GPL v2.0,
  * pursuant to article 5.3.4 of the CeCILL v.2.1.
@@ -18,7 +18,6 @@
 #include "scoUtils.h"
 
 #include "sci_malloc.h"
-#include "elementary_functions.h"
 #include "string.h"
 
 #include "getGraphicObjectProperty.h"
