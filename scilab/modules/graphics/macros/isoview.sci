scilab/scilab

[graphics] actual removal of isoview(xmin,ymin,xmax,ymax) syntax
  Code already cleaned up @ https://codereview.scilab.org/#/c/18166/

Change-Id: I247f9829ce2db292d76049b4cd11aff0df14b0a5

scilab/modules/graphics/macros/isoview.sci
@@ -1,7 +1,7 @@
 // Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
 // Copyright (C) INRIA
 // Copyright (C) 2010 - DIGITEO - Pierre Lando <pierre.lando@scilab.org>
-// Copyright (C) 2016 - Samuel GOUGEON
+// Copyright (C) 2016, 2017 - Samuel GOUGEON
 //
 // Copyright (C) 2012 - 2016 - Scilab Enterprises
 //
@@ -13,21 +13,20 @@
 // along with this program.
 
 function isoview(varargin)
-    //isoview
-    //isoview on
-    //isoview off
-    //isoview(idGraphics [,"on"|"off"])
-    //isoview(xmin, xmax, ymin, ymax)  // OBSOLETE
+    // isoview
+    // isoview on
+    // isoview off
+    // isoview(idGraphics [,"on"|"off"])
 
     rhs = argn(2)
-    if rhs==3 | rhs>4 then
-        msg = _("%s: Wrong number of input argument(s): 0, 1, 2, or 4 arguments expected.\n")
+    if rhs>2 then
+        msg = _("%s: Wrong number of input argument(s): 0, 1, or 2 arguments expected.\n")
         error(msprintf(msg, "isoview"));
     end
 
     // isoview action to be performed: ""=switch, "on", "off"
     action = ""
-    if rhs>0 & rhs<4 & typeof(varargin($))=="string" then
+    if rhs>0 & typeof(varargin($))=="string" then
         tmp = convstr(varargin($))
         if tmp(1)=="on"
             action = "on"
@@ -38,9 +37,9 @@ function isoview(varargin)
 
     // Set of axes to be processed
     // ---------------------------
-    if rhs==0 | rhs==4 | (rhs==1 & type(varargin(1))==10)
+    if rhs==0 | (rhs==1 & type(varargin(1))==10)
         a = gca()
-    elseif rhs<3 // varargin(1) must be a graphics handle
+    else // varargin(1) must be a graphics handle
         a = varargin(1)
         if type(a)~=9 then
             msg = _("%s: Wrong type for input argument #%d: Graphic handle or string expected.\n")
@@ -69,20 +68,6 @@ function isoview(varargin)
         end
     end
 
-    // isoview(xmin, xmax, ymin, ymax): checking input bounds
-    // ------------------------------------------------------
-    if rhs>2
-        msg = _("isoview: The syntax isoview(xmin, xmax, ymin, ymax) will be removed from Scilab 6.1\nPlease use replot([xmin xmax ymin ymax]) after isoview(..) instead.\n")
-        warning(msprintf(msg))
-        for i = 1:4
-            if type(varargin(i)) <> 1 then
-                msg = _("%s: Wrong type for input argument #%d: A real expected.\n")
-                error(msprintf(msg, "isoview", i))
-            end
-        end;
-        [xmin xmax ymin ymax] = varargin(1:4)
-    end
-
     // Performing actions
     // ------------------
     for i = 1:size(a,"*")
@@ -95,9 +80,6 @@ function isoview(varargin)
             a(i).isoview = "off"
         end
         a(i).clip_state = "clipgrf"
-        if rhs>2 then
-            a(i).data_bounds = [xmin xmax ymin ymax]
-        end
     end
 endfunction
 

rdbyk/balisc

Conflicts:
	scilab/modules/graphics/macros/isoview.sci

scilab/modules/graphics/macros/isoview.sci
@@ -1,9 +1,9 @@
 // Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
 // Copyright (C) INRIA
 // Copyright (C) 2010 - DIGITEO - Pierre Lando <pierre.lando@scilab.org>
-// Copyright (C) 2016 - Samuel GOUGEON
+// Copyright (C) 2016, 2017 - Samuel GOUGEON
 // Copyright (C) 2012 - 2016 - Scilab Enterprises
-// Copyright (C) 2018 - Dirk Reusch, Kybernetik Dr. Reusch
+// Copyright (C) 2018 - 2020 Dirk Reusch, Kybernetik Dr. Reusch
 //
 // This file is hereby licensed under the terms of the GNU GPL v2.0,
 // pursuant to article 5.3.4 of the CeCILL v.2.1.
@@ -13,20 +13,15 @@
 // along with this program.
 
 function isoview(varargin)
-    //isoview
-    //isoview on
-    //isoview off
-    //isoview(idGraphics [,"on"|"off"])
-    //isoview(xmin, xmax, ymin, ymax)  // OBSOLETE
 
-    if nargin==3 | nargin>4 then
-        msg = _("%s: Wrong number of input arguments: 0, 1, 2, or 4 arguments expected.\n")
+    if nargin>2 then
+        msg = _("%s: Wrong number of input argument(s): 0, 1, or 2 arguments expected.\n")
         error(msprintf(msg, "isoview"));
     end
 
     // isoview action to be performed: ""=switch, "on", "off"
     action = ""
-    if nargin>0 & nargin<4 & typeof(varargin($))=="string" then
+    if nargin>0 & typeof(varargin($))=="string" then
         tmp = convstr(varargin($))
         if tmp(1)=="on"
             action = "on"
@@ -37,9 +32,9 @@ function isoview(varargin)
 
     // Set of axes to be processed
     // ---------------------------
-    if nargin==0 | nargin==4 | (nargin==1 & type(varargin(1))==10)
+    if nargin==0 | (nargin==1 & type(varargin(1))==10)
         a = gca()
-    elseif nargin<3 // varargin(1) must be a graphics handle
+    else // varargin(1) must be a graphics handle
         a = varargin(1)
         if type(a)~=9 then
             msg = _("%s: Wrong type for input argument #%d: Graphic handle or string expected.\n")
@@ -68,20 +63,6 @@ function isoview(varargin)
         end
     end
 
-    // isoview(xmin, xmax, ymin, ymax): checking input bounds
-    // ------------------------------------------------------
-    if nargin>2
-        msg = _("isoview: The syntax isoview(xmin, xmax, ymin, ymax) will be removed from Scilab 6.1\nPlease use replot([xmin xmax ymin ymax]) after isoview(..) instead.\n")
-        warning(msprintf(msg))
-        for i = 1:4
-            if type(varargin(i)) <> 1 then
-                msg = _("%s: Wrong type for input argument #%d: A real expected.\n")
-                error(msprintf(msg, "isoview", i))
-            end
-        end;
-        [xmin xmax ymin ymax] = varargin(1:4)
-    end
-
     // Performing actions
     // ------------------
     for i = 1:size(a,"*")
@@ -94,9 +75,5 @@ function isoview(varargin)
             a(i).isoview = "off"
         end
         a(i).clip_state = "clipgrf"
-        if nargin>2 then
-            a(i).data_bounds = [xmin xmax ymin ymax]
-        end
     end
 endfunction
-