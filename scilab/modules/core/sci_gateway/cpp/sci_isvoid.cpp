195408c9b5bbf2d6af367679ed41fa353a16d63b
rdbyk <rdbyk@users.noreply.github.com>
2021-04-08 17:32:50 +0200

    add forgotten sci_isvoid.cpp

---
+++ scilab/modules/core/sci_gateway/cpp/sci_isvoid.cpp
@@ -0,0 +1,48 @@
+// Balisc (https://github.com/rdbyk/balisc/)
+//
+// Copyright (C) 2021 - Dirk Reusch, Kybernetik Dr. Reusch
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+// 02110-1301, USA.
+
+#include "core_gw.hxx"
+#include "function.hxx"
+#include "bool.hxx"
+#include "listundefined.hxx"
+
+extern "C"
+{
+#include "Scierror.h"
+#include "localization.h"
+}
+
+using types::Bool;
+using types::Function;
+using types::ListUndefined;
+using types::typed_list;
+
+Function::ReturnValue sci_isvoid(typed_list &in, int _iRetCount, typed_list &out)
+{
+	if (in.size() == 1)
+	{
+		out.push_back(new Bool(in[0]->isListUndefined()));
+		return Function::OK;
+	}
+	else
+	{
+		Scierror(71, 1);
+		return Function::Error;
+	}
+}
