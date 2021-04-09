991d6e66e7d12b667a0f3815b02dd98e99ef1708
rdbyk <rdbyk@users.noreply.github.com>
2021-04-09 16:35:11 +0200

    number_properties was broken (missing include in C gateway)

--- scilab/modules/elementary_functions/sci_gateway/c/sci_number_properties.c
+++ scilab/modules/elementary_functions/sci_gateway/c/sci_number_properties.c
@@ -2,7 +2,7 @@
  * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
  * Copyright (C) 2006 - INRIA - Allan CORNET
  * Copyright (C) 2012 - 2016 - Scilab Enterprises
- * Copyright (C) 2017 - 2019 Dirk Reusch, Kybernetik Dr. Reusch
+ * Copyright (C) 2017 - 2021 Dirk Reusch, Kybernetik Dr. Reusch
  * 
  * This file is hereby licensed under the terms of the GNU GPL v2.0,
  * pursuant to article 5.3.4 of the CeCILL v.2.1.
@@ -19,6 +19,7 @@
 #include "api_scilab.h"
 #include "Scierror.h"
 #include "strcmp.h"
+#include "numericconstants_interface.h"
 
 int sci_number_properties(char *fname, void* pvApiCtx)
 {
