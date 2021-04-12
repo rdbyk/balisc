1a9e94d2b8428f2a172507ead7a2abcdaccf06ad
rdbyk <rdbyk@users.noreply.github.com>
2021-04-12 16:17:23 +0200

    sci_exec.cpp: fix Scierror call

--- scilab/modules/functions/sci_gateway/cpp/sci_exec.cpp
+++ scilab/modules/functions/sci_gateway/cpp/sci_exec.cpp
@@ -2,7 +2,7 @@
  * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
  * Copyright (C) 2006 - INRIA - Antoine ELIAS
  * Copyright (C) 2012 - 2016 - Scilab Enterprises
- * Copyright (C) 2017 - 2019 Dirk Reusch, Kybernetik Dr. Reusch
+ * Copyright (C) 2017 - 2021 Dirk Reusch, Kybernetik Dr. Reusch
  *
  * This file is hereby licensed under the terms of the GNU GPL v2.0,
  * pursuant to article 5.3.4 of the CeCILL v.2.1.
@@ -145,7 +145,7 @@ types::Function::ReturnValue sci_exec(types::typed_list &in, int _iRetCount, typ
         else
         {
             //not managed
-            Scierror(90, 2, _("%integer or a string"), "exec", 2);
+            Scierror(90, 2, _("%integer or a string"));
             return types::Function::Error;
         }
     }
